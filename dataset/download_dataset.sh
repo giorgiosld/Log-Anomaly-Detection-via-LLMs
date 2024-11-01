ee3e197184b72c2b0e3284c2087ca2f16d2828fd
giorgio <giorgiosaldana99@gmail.com>
2024-11-01 13:03:05 +0000

    [edit] Edit .gitignore

---
+++ dataset/download_dataset.sh
@@ -0,0 +1,7 @@
+# To download the HDFS dataset, run the following command:
+wget -O HDFS_v1.zip https://zenodo.org/records/8196385/files/HDFS_v1.zip?download=1
+# Extract the zip file
+unzip HDFS_v1.zip -d HDFS_v1
+# Remove the zip file
+rm HDFS_v1.zip
+
