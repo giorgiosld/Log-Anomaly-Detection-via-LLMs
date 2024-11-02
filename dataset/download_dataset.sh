d766d00763775fe69201e2f87103d1f8b5b5aa84
giorgio <giorgiosaldana99@gmail.com>
2024-11-02 11:44:53 +0000

    [edit] Edit script to download the dataset

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,6 +1,6 @@
 # To download the HDFS dataset, run the following command:
-wget -O HDFS_v1.zip https://zenodo.org/records/8196385/files/HDFS_v1.zip?download=1
+wget -O HDFS_v1.zip https://zenodo.org/records/8196385/files/HDFS_v1.zip?download=1 && \
 # Extract the zip file
-unzip HDFS_v1.zip -d HDFS_v1
+unzip HDFS_v1.zip -d HDFSv1 && \
 # Remove the zip file
 rm HDFS_v1.zip
