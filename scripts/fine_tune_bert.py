a9665f813542a73a32cb46057ce0a2c7d6b63a89
giorgio <giorgiosaldana99@gmail.com>
2024-11-02 12:16:42 +0000

    [edit] Edit script to fix the correct path for dataset and added print to have a better visualization of steps done and in progress during fine-tune

--- scripts/fine_tune_bert.py
+++ scripts/fine_tune_bert.py
@@ -2,9 +2,14 @@ from models.bert.dataset import load_data, prepare_datasets, tokenize_data
 from models.bert.train import train_model
 
 if __name__ == "__main__":
-    npz_path = "dataset/HDFSv1/HDFS.npz"
+    print("Starting fine tuning BERT model..")
+    npz_path = "dataset/HDFSv1/preprocessed/HDFS.npz"
     x_data_str, y_data = load_data(npz_path)
+    print("Loaded data..")
     train_texts, val_texts, train_labels, val_labels = prepare_datasets(x_data_str, y_data)
+    print("Prepared datasets..")
     train_encodings, val_encodings = tokenize_data(train_texts, val_texts)
+    print("Tokenized data..")
+    print("Starting SFT model..")
     train_model(train_encodings, train_labels, val_encodings, val_labels)
 
