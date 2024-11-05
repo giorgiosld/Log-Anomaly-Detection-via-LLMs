b5d14e93bd49f946b13c581047c7e432b74b478b
giorgiosld <giorgiosaldana99@gmail.com>
2024-11-05 16:18:30 +0000

    [add] Add script bash to start job within HPC

---
+++ run_fine_tune_llama.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=log_anomaly_detection_sft_llama
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=giorgio24@ru.is
+#SBATCH --partition=gpu-1xA100
+#SBATCH --ntasks-per-node=48
+#SBATCH --mem=64G
+#SBATCH --time=0-09:00:00         
+#SBATCH --hint=nomultithread    
+#SBATCH --output=/hpchome/giorgio24/Log-Anomaly-Detection-via-LLMs/logs/llama_fine_tune_output.out
+#SBATCH --error=/hpchome/giorgio24/Log-Anomaly-Detection-via-LLMs/logs/llama_fine_tune_errors.err
+#SBATCH --chdir=/hpchome/giorgio24/Log-Anomaly-Detection-via-LLMs
+
+# Loading necessary modules
+ml load Python
+ml use /opt/ohpc/pub/modulefiles
+ml load nvidia/nvhpc/22.3
+ml load NVHPC/23.7-CUDA-12.1.1
+ml load Anaconda3/2023.09-0
+
+# Activate the Conda environment
+source activate /hpchome/giorgio24/Log-Anomaly-Detection-via-LLMs/conda_venv
+
+# Verify the environment is activated
+echo "Environment activated: $CONDA_PREFIX"
+
+# Set environment variables
+export PYTHONPATH=/hpchome/giorgio24/Log-Anomaly-Detection-via-LLMs
+
+# Run the fine-tun script
+python scripts/fine_tune_llama.py
